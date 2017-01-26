583a2f81169c743a004be6e796f8a1627f813bde
john doe <scottbruceburton@gmail.com>
2017-01-25 23:19:51 -0500

    fixed NULL rotation error and added comments

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import caesar
 import cgi
 
 def build_page(textAreaContent):
+    # message variables
     mes_label = '<label>What would you like to encrypt: </label><br>'
-    rot_label = '<label>By how many would you like to rotate:</label>'
     textArea = "<textarea name='message' style='width:40%;height:15%'>"+textAreaContent+"</textarea>"
+    # rotation variables
+    rot_label = '<label>By how many would you like to rotate:</label>'
     rotation_input = "<input name='rotation' type='number' style='margin-left:2em; width:50px'/>"
+    # submit button
     submit = "<input type='submit'/>"
+
+    # form action=which page it goes to
+    # form method=get or post
     form = "<form action='.' method='post'>" + mes_label + textArea + "<br><br>" + rot_label + rotation_input + "<br><br>" + submit + "</form>"
 
     header = "<h2>Web Caesar</h2>"
@@ -16,13 +22,17 @@ def build_page(textAreaContent):
 
 class MainHandler(webapp2.RequestHandler):
 
+    # initial build page of '/'
     def get(self):
         content = build_page("")
         self.response.write(content)
 
     def post(self):
         message = self.request.get("message")
-        rotation = int(self.request.get("rotation"))
+        if self.request.get("rotation") == '':
+            rotation = 0
+        else:
+            rotation = int(self.request.get("rotation"))
         encrypted_message = caesar.encrypt(message, rotation)
         escaped_encrypted = cgi.escape(encrypted_message)
         content = build_page(escaped_encrypted)
